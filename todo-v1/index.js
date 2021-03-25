53775ea7385f40d5d850de176c3409ac0b937944
Eduardo Flores <efloresambrosio@gmail.com>
2021-03-25 03:08:22 -0600

    chg: refactor DB info to ES6 syntax

--- todo-v1/index.js
+++ todo-v1/index.js
@@ -1,13 +1,5 @@
 import express from 'express';
-
-//TODO move to a fixture or something similar
-const todos = [
-    {
-        id: 1,
-        description: "Awesome task",
-        title: "lunch"
-    }
-];
+import db from './src/db/todos'
 
 // Setup our app
 const app = express();
@@ -15,7 +7,7 @@ const port = 3000;
 
 app.get('/api/todos', (req, res) => {
     res.status(200).send({
-        success: 'true', message: 'todos retrieved successfully', todos: todos
+        success: 'true', message: 'todos retrieved successfully', todos: db
     })
 });
 
