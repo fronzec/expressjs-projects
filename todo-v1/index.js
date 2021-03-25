bd4902bb63c7886a8499dc520d23fb435c4a5e65
Eduardo Flores <efloresambrosio@gmail.com>
2021-03-25 02:48:02 -0600

    new: first endpoint that returns dummie todo info

--- todo-v1/index.js
+++ todo-v1/index.js
@@ -1,6 +1,23 @@
 const express = require('express');
+
+//TODO move to a fixture or something similar
+const todos =  [
+    {
+        id: 1,
+        description: "Awesome task",
+        title: "lunch"
+    }
+];
+
+// Setup our app
 const app = express();
 const port = 3000;
+
+app.get('/api/todos', (req, res) => {
+    res.status(200).send({
+        success: 'true', message: 'todos retrieved successfully', todos: todos
+    })
+});
 
 app.listen(port, () => {
     console.info(`Service started in port ${port}`);
