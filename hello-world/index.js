d9e32852687e77fede2bf13391b05d5084f1dd78
Eduardo Flores <efloresambrosio@gmail.com>
2021-03-25 00:54:15 -0600

    chg: comments to learn

--- hello-world/index.js
+++ hello-world/index.js
@@ -3,6 +3,11 @@ const app = express();
 const port = 3000;
 
 // We define a basic route / usually called root URL. Other paths will respond with 404
+// Routing in express follows app.METHOD(PATH, HANDLER)
+// Where:
+//  METHOD is the HTTP method
+//  PATH is a path in the server
+//  HANDLED is the function executed when the route is matched
 app.get('/', (req, res) => {
     console.debug("[ROOT]:: Incoming request");
     res.send('Hello World from ExpressJs!');
